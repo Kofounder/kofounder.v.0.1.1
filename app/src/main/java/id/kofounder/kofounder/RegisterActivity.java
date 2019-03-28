ce0cc718e0d586581572a9df43c33c1ae867154e
kofounder <kofounder.info@gmail.com>
2019-03-28 15:15:15 +0800

    Phase 3

--- app/src/main/java/id/kofounder/kofounder/RegisterActivity.java
+++ app/src/main/java/id/kofounder/kofounder/RegisterActivity.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText edt_username, edt_fullname, edt_email, edt_password;
+    EditText edt_username, edt_fullname, edt_email, edt_nohp, edt_password;
     Button btn_register;
     TextView txt_login;
 
@@ -40,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
         edt_username = findViewById(R.id.edt_username);
         edt_fullname = findViewById(R.id.edt_fullname);
         edt_email = findViewById(R.id.edt_email);
+        edt_nohp = findViewById(R.id.edt_nohp);
         edt_password = findViewById(R.id.edt_password);
         btn_register = findViewById(R.id.btn_register);
         txt_login = findViewById(R.id.txt_login);
@@ -61,22 +62,23 @@ public class RegisterActivity extends AppCompatActivity {
                 progressDialog.show();
 
                 String str_username = edt_username.getText().toString();
-                String str_fullname = edt_fullname.getText().toString();
                 String str_email = edt_email.getText().toString();
+                String str_fullname = edt_fullname.getText().toString();
+                String str_nohp = edt_nohp.getText().toString();
                 String str_password = edt_password.getText().toString();
 
-                if (TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)){
+                if (TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_nohp) || TextUtils.isEmpty(str_password)){
                     Toast.makeText(RegisterActivity.this, " All field are required!", Toast.LENGTH_SHORT).show();
                 } else if (str_password.length() < 6){
                     Toast.makeText(RegisterActivity.this, " Password must have 6 characters!", Toast.LENGTH_SHORT).show();
                 } else {
-                    btn_register(str_username, str_fullname, str_email, str_password);
+                    btn_register(str_username, str_fullname, str_email, str_nohp, str_password);
                 }
             }
         });
     }
 
-    public void btn_register(final String edt_username, final String edt_fullname, String edt_email, String edt_password){
+    public void btn_register(final String edt_username, final String edt_fullname, String edt_email, String edt_nohp, String edt_password){
         auth.createUserWithEmailAndPassword(edt_email, edt_password)
                 .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>(){
                     @Override
--- app/src/main/java/id/kofounder/kofounder/StartActivity.java
+++ app/src/main/java/id/kofounder/kofounder/StartActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,6 +13,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class StartActivity extends AppCompatActivity {
 
     Button btn_register;
+    TextView txt_login;
 
     FirebaseUser firebaseUser;
 
@@ -35,6 +37,14 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         btn_register = findViewById(R.id.btn_register);
+        txt_login = findViewById(R.id.txt_login);
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
